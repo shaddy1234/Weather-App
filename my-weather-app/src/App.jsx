5efc175421c54dabea4d499034f6a445425be308
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-01-18 22:31:50 +0300

    Running the code on app

--- my-weather-app/src/App.jsx
+++ my-weather-app/src/App.jsx
@@ -1,13 +1,13 @@
 // // src/App.jsx
-// import React from 'react';
-// import Weather from './components/Weather';
+import React from 'react';
+import Weather from './components/Weather';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Weather />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
