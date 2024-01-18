9563533d73e89e271963b3770d15ddb8d34ee465
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-01-18 16:14:01 +0300

    Initial Setup

---
+++ my-weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+// // src/App.jsx
+// import React from 'react';
+// import Weather from './components/Weather';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Weather />
+//     </div>
+//   );
+// }
+
+// export default App;
